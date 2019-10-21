08a07d5724b48cf44006968bd3caaa02224e00c7
Natasha Shabir <1016761@gmail.com>
2019-10-22 07:39:15 +1100

    Update on Touch inputs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 40f;
     private float horizontalMove = 0f;
     private bool jump = false;
+   // private Vector3 touchPosition;
+
+    public Rigidbody2D myRigidbody;
 
     // Update is called once per frame
     void Update()
@@ -17,13 +20,50 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump"))
         {
-            jump = true;           
+            jump = true;
         }
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+            Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
+            if (Input.touchCount == 1)
+            {
+                if (touchPosition != transform.position)
+                {
+                    float x = touchPosition.x;
+                    float y = touchPosition.y;
+
+                    if (Mathf.Abs(x) > Mathf.Abs(y))
+                    {
+                        if (x < 0)
+                        {
+                            touchPosition += Vector3.left;
+                        }
+                        else
+                        {
+                            touchPosition += Vector3.right;
+                        }
+
+                        transform.position = Vector3.MoveTowards(transform.position, touchPosition, runSpeed * Time.fixedDeltaTime);
+                    }
+
+                }
+            }
+        }
+
+        //if (Input.touchCount > 0)
+        //{
+        //    Touch touch = Input.GetTouch(0);
+        //    Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+        //    touchPosition.z = 0f;
+
+        //}
     }
 
     private void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
-        jump = false;        
+        jump = false;
     }
 }
---
+++ Assets/Scripts/Swipe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour
+{
+    private bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+    private Vector2 startTouch, swipeDelta;
+
+    private void Update()
+    {
+        tap = false;
+        swipeLeft = false;
+        swipeRight = false;
+        swipeUp = false;
+        swipeDown = false;
+                
+    }
+
+    private void Reset()
+    {
+        startTouch = swipeDelta = Vector2.zero;
+    }
+
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+    public bool SwipeLeft { get { return swipeLeft; } }
+    public bool SwipeRight { get { return swipeRight; } }
+    public bool SwipeUp { get { return swipeUp; } }
+    public bool SwipeDown { get { return swipeDown; } }
+
+    
+
+
+}
