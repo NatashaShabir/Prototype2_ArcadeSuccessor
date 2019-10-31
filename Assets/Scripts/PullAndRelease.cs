e9acb495fc9e2449d4be4cb9fb61a8cdad69c012
Jacob Clements <1014906@student.sae.edu.au>
2019-10-31 16:21:08 +1100

    Added shooting mechanic
    Needs to go through walls but hit lumberjack

---
+++ Assets/Scripts/PullAndRelease.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullAndRelease : MonoBehaviour
+{
+    Vector2 startPos;
+    //force added upon release
+    public float force = 1300f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        //disable kinematic
+        GetComponent<Rigidbody2D>().isKinematic = false;
+
+        //add the force
+        Vector2 dir = startPos - (Vector2)transform.position;
+        GetComponent<Rigidbody2D>().AddForce(dir * force);
+
+        //remove this script
+        Destroy(this);
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //keep in a certain radius
+        float radius = 0.5f;
+        Vector2 dir = p - startPos;
+        if (dir.sqrMagnitude > radius)
+            dir = dir.normalized * radius;
+
+        //set the position
+        transform.position = startPos + dir;
+    }
+
+}
---
+++ Assets/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    //banana prefab to spawn
+    public GameObject bananaPrefab;
+    //is there a banana in the area?
+    bool occupied = false;
+
+    void FixedUpdate()
+    {
+        //bird not in trigger area anymore?
+        if (!occupied)
+            spawnNext();
+    }
+
+    void spawnNext()
+    {
+        //spawn a banana at current position
+        Instantiate(bananaPrefab, transform.position, Quaternion.identity);
+        occupied = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        //banana left the monkey
+        occupied = false;
+    }
+
+}
